45fd4af9a4aa0951e750c7441dda6d8c0d0bcf91
Ayush8709 <ayushsingh0455@gmail.com>
2024-09-29 01:39:23 -0700

    multipal component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
 export default function Home() {
   return (
     <main >
-      <h1>Event, function and state</h1>
+      <h1>multipal conmponent</h1>
+      <User name='singh'/>
+      <Name name='ayush'/>
     </main>
 
   );
+}
+
+
+const User = (props) => {
+  return (
+    <h1>This is User {props.name}</h1>
+  )
+}
+
+
+const Name = ({name})=>{
+  return (
+    <h1>This is Name {name}</h1>
+  )
 }
 
 
