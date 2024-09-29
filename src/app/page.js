9a61bb76282b64c92545e70b9ae860d8007441f1
Ayush8709 <ayushsingh0455@gmail.com>
2024-09-29 01:22:26 -0700

    Routing in next js

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,11 @@
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Home Page</h1>
-      <User name="Ayush" />
-      <ThiredComponent third="3" />
-      <ThiredComponent />
+    <main >
+      <h1>Event, function and state</h1>
     </main>
 
   );
 }
 
-const User = ({ name }) => {
-  return (
-    <div>
-      <h2>Name is {name}</h2>
-    </div>
-  )
-}
 
-const ThiredComponent = (props) => {
-  return (
-    <div>
-      <h1>This is Third {props.third} Component</h1>
-    </div>
-  )
-}
+
